dfee7e1a008d46f39094c7e6231a30163cd57bb6
meetalbiorix <meetv.albiorix@gmail.com>
2023-12-26 13:10:26 +0530

    Install tailwind and  pinia

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <main >
+    <h1 class="text-3xl font-bold underline">{{ website.name }}</h1>
+    <p>{{ website.description }}</p>
+    <button @click="website.changeName">Change name</button>
+  </main>
+</template>
+
+<script setup>
+const website = useWebsiteStore()
+
+await callOnce(website.fetch)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt"],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
+})
